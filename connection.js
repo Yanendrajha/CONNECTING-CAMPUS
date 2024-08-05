6c49e8225139a9431ac8bcaa179b7ea72de031c6
Yanendrajha <yanendrajha37@gmail.com>
2024-08-05 14:00:56 +0530

    First Commit

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const url= "mongodb+srv://yanendrajha37:Xmv945XiyyYgJtVY@cluster0.ayi7fwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/posting"
+require('dotenv').config();
 
+const url = "mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@cluster0.ayi7fwq.mongodb.net/posting"
 async function connectionMongoDB(){
     return mongoose.connect(url).then(() => {
         console.log("connected to mongodb");
